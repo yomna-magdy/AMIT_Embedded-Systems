1ca225e68dd68a7f399081140421a2f326933c2d
Yomna Magdy <yomnaaaa2@gmail.com>
2025-06-09 20:18:04 +0300

    VR 2

--- LED_Test/LED_Test/main.c
+++ LED_Test/LED_Test/main.c
@@ -7,12 +7,26 @@
 
 #include <avr/io.h>
 
+/*UTILES_LIB*/
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+
+/*MCAL*/
+#include "DIO_interface.h"
+
+/*HAL*/
+#include "LED_interface.h"
+
+
 
 int main(void)
 {
+	 DIO_voidSetPinDirection(PORTC, DIO_PIN2 , DIO_PIN_OUTPUT);
+	 
     /* Replace with your application code */
     while (1) 
     {
+		DIO_voidSetPinValue(PORTC,DIO_PIN2 , DIO_PIN_HIGH);
     }
 }
 
